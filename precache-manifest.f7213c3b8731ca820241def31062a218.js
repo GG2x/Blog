1bec2d10e920da660c6ae4a866b7bc215f466aa7
GG2x <sanchez.jerem@gmail.com>
2020-02-04 16:12:13 +0100

    Updates

---
+++ precache-manifest.f7213c3b8731ca820241def31062a218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c8a729e6c07a90e867e020d0918e7d",
+    "url": "/blogstartreact/index.html"
+  },
+  {
+    "revision": "b280f662dad17e7271e3",
+    "url": "/blogstartreact/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bac94bcd4e9a86427280",
+    "url": "/blogstartreact/static/css/main.07de5f80.chunk.css"
+  },
+  {
+    "revision": "b280f662dad17e7271e3",
+    "url": "/blogstartreact/static/js/2.75624e1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blogstartreact/static/js/2.75624e1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bac94bcd4e9a86427280",
+    "url": "/blogstartreact/static/js/main.ec53545b.chunk.js"
+  },
+  {
+    "revision": "838d0d28e6a3d5219e51",
+    "url": "/blogstartreact/static/js/runtime-main.c6ce2b44.js"
+  }
+]);
